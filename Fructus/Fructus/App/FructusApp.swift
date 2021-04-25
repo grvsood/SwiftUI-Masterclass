ca2cebe6a1cd32cfaff509768f57a5668a84cf2d
Gaurav Sood <soodg@amazon.com>
2021-04-25 14:40:11 +0530

    Fructus

---
+++ Fructus/Fructus/App/FructusApp.swift
@@ -0,0 +1,20 @@
+//
+//  Created by Sood, Gaurav on 25/04/21.
+//
+
+import SwiftUI 
+
+@main
+struct FructusApp: App {
+  @AppStorage("isOnboarding") var isOnboarding: Bool = true
+  
+  var body: some Scene {
+    WindowGroup {
+      if isOnboarding {
+        OnboardingView()
+      } else {
+        ContentView()
+      }
+    }
+  }
+}
---
+++ Fructus/Fructus/View/SettingsLabelView.swift
@@ -0,0 +1,32 @@
+//
+//  Created by Sood, Gaurav on 25/04/21.
+//
+
+import SwiftUI
+
+struct SettingsLabelView: View {
+  // MARK: - PROPERTIES
+  
+  var labelText: String
+  var labelImage: String
+
+  // MARK: - BODY
+
+  var body: some View {
+    HStack {
+      Text(labelText.uppercased()).fontWeight(.bold)
+      Spacer()
+      Image(systemName: labelImage)
+    }
+  }
+}
+
+// MARK: - PREVIEW
+
+struct SettingsLabelView_Previews: PreviewProvider {
+  static var previews: some View {
+    SettingsLabelView(labelText: "Fructus", labelImage: "info.circle")
+      .previewLayout(.sizeThatFits)
+      .padding()
+  }
+}
